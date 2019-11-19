1e9093b8563d27e5ff9dc3920833f3a22b0045df
loney <geo.giatrakos@gmail.com>
2019-11-19 21:02:19 +0200

    Added k to variables
    + Added k = number of clusters to variables of Clustering Class

--- src/Clustering.cpp
+++ src/Clustering.cpp
@@ -8,11 +8,21 @@
 
 #include "Clustering.hpp"
 
-Clustering::Clustering(Database *db,int init_f,int assign_f,int update_f) : db(db) {
+Clustering::Clustering(Database *db,int num_of_clusters ,int init_f,int assign_f,int update_f) : db(db) , k(num_of_clusters){
     flags[0] = init_f ;
     flags[1] = assign_f ;
     flags[2] = update_f ;
+    distribution = new std::uniform_int_distribution<int> (0,db->getSize() - 1); // We produce number in the
 }
+void Clustering::random_init(void) {
+
+}
+void Clustering::kmeans_init(void) {}
+void Clustering::lloyd_assign(void) {}
+void Clustering::range_search_assign(void) {}
+void Clustering::pam_update(void) {}
+void Clustering::mean_update(void){}
+
 void Clustering::init(void) {
     if (flags[0] == 0) {
         random_init();
--- src/Clustering.hpp
+++ src/Clustering.hpp
@@ -20,15 +20,18 @@ using namespace std ;
 
 class Clustering {
 private:
-    void init() ; // Does the initialization of the k centers
-    void assign(); // Does the assignment of the n vectors to their clusters
-    void update(); // Does the update of the k centers
     void random_init(); // Random selection of K items
     void kmeans_init(); // K-means++ selectionof first K items
     void lloyd_assign(); // Lloyd assignment
     void range_search_assign(); // Assignment by Range search
     void pam_update(); // Update using PAM (lloyds)
     void mean_update(); // Update using mean vectors / curve
+
+    void init() ; // Does the initialization of the k centers
+    void assign(); // Does the assignment of the n vectors to their clusters
+    void update(); // Does the update of the k centers
+
+    int k ; // The number of clusters k
 
     Database *db ; // Database with points
     int flags[3] ; // Flags used to determine which algorithms are used
@@ -39,13 +42,13 @@ private:
     // representative center
     std::map<string, int> assigned; // Map that matches each item to its cluster
 
-    std::uniform_int_distribution<int> distribution ; // Uniform distribution used for
+    std::uniform_int_distribution<int> *distribution ; // Uniform distribution used for
     // random select
     std::default_random_engine generator;
 
 public:
-    Clustering(Database *,int ,int ,int ); // Database with data points
-    // Followed by sequence of [0,1,0] etc that selects the init , assign and update
+    Clustering(Database *,int ,int ,int ,int ); // Database with data points
+    // Followed by k and sequence of [0,1,0] etc that selects the init , assign and update
     // algorithms respectively
     void runClustering(); // Function called by main
     ~Clustering();
