5bf30b721823ab00a92434b8298926f11085d840
loney <geo.giatrakos@gmail.com>
2019-12-05 19:59:00 +0200

    Add thread code
    + Update main with code for threading

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,23 @@
 #include "CMDParser.hpp"
+#include "ConfParser.hpp"
 #include <string>
 #include <iostream>
+#include <unistd.h>
+#include <thread>
 
+
+struct cluster_t_args {
+    cluster_t_args(){}
+    bool isCurve ;
+    int k ;
+    unsigned int flags[3];
+    //std::ostream & out ;
+};
+
+void cluster_thread_f(cluster_t_args args) {
+    //cluster_t_args * args = ( *) _args ;
+    std::cout << args.isCurve << " " << args.k << " " << args.flags[0] << args.flags[1] << args.flags[2] << std::endl ;
+}
 
 int main(int argc, char **argv) {
     CMDParser cmd_p ;
@@ -10,5 +26,29 @@ int main(int argc, char **argv) {
     std::string output ;
     cmd_p.getArgs(argc,argv,input,config,output);
     std::cout << input << " " << config << " " << output << "\n" ;
+    ConfParser conf_p (config) ;
+    int nclusters ;
+    int ngrids ;
+    int nvht ;
+    int nvfs ;
+    conf_p.parseConfig(nclusters,ngrids,nvht,nvfs);
+    //std::thread * cluster_ts = new std::thread[8] ;
+    for (unsigned int i = 0; i < 8; i++) {
+        int err ;
+        int mask1 = 1 << 0 ;
+        int mask2 = 1 << 1 ;
+        int mask3 = 1 << 2 ;
+
+        struct cluster_t_args args ;
+        args.flags[0] = (i & mask1) >> 0;
+        args.flags[1] = (i & mask2) >> 1;
+        args.flags[2] = (i & mask3) >> 2;
+
+        args.isCurve = false ;
+        args.k = nclusters ;
+        //args->out = std::cout ;
+        std::thread t(cluster_thread_f,args);
+        t.join();
+    }
     return 0;
 }
